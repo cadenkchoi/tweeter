1b12fccff9d5749b07f53d6c01a9bfbdb9361567
Caden Choi <cadenc.7@gmail.com>
2021-03-24 21:14:18 +0000

    added functionality to client js to take data from new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,67 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      let newTweets = createTweetElement(tweet);
+      $('.tweet-container').append(newTweets);
+    }
+  }
+
+  const createTweetElement = function(tweet) {
+    const $tweet = (`
+    <article class="tweet">
+    <header class="names1">
+      <div class="pic-name">
+        <img class="tweet-img" src="${tweet.user.avatars}" alt="">
+        <h4 class="tweet-username">${tweet.user.name}</h4>
+      </div>
+      <h4 class="tweet-handle">${tweet.user.handle}</h4>
+    </header>
+    <span class="tweet-body">${tweet.content.text}</span>
+    <footer class="tweet-footer">
+      <h4 class="tweet-timestamp">${tweet.created_at}</h4>
+      <div class="tweet-icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+    </article>
+    `);
+    // ...
+    return $tweet;
+  }
+
+  renderTweets(data);
+
+});
+
