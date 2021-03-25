b04fc087292b63911499f254d60382e23943f785
Caden Choi <cadenc.7@gmail.com>
2021-03-25 01:02:38 +0000

    made changes to the js in order for it to alert if tweet is empty or over 140 chars, also renders tweets async without refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,36 +7,34 @@
 // Fake data taken from initial-tweets.json
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+  $(".compose-tweet").submit(function(event){
+    event.preventDefault();
+    let tweetMsg = $('.compose-tweet').serialize();
+    if(!$('#tweet-text').val()) {
+      alert('Empty Tweet! Please Try Again!');
+    } else if ($('#tweet-text').val().length > 140) {
+      alert('Please type less than 140 characters!')
+    } else {
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: tweetMsg,
+      })
+      .then(loadTweets);
     }
-  ]
+  });
 
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       let newTweets = createTweetElement(tweet);
-      $('.tweet-container').append(newTweets);
+      $('.tweet-container').prepend(newTweets);
     }
+  }
+
+  const loadTweets = function() {
+    $.get("/tweets", function(res) {
+      renderTweets(res);
+    })
   }
 
   const createTweetElement = function(tweet) {
@@ -60,11 +58,7 @@ $(document).ready(function() {
     </footer>
     </article>
     `);
-    // ...
     return $tweet;
   }
-
-  renderTweets(data);
-
 });
 
