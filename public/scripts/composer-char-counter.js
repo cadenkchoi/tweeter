3d8fccd817e20d5249b73ff0e0e7b57b2c0b5835
Caden Choi <cadenc.7@gmail.com>
2021-03-26 01:39:37 +0000

    removed commented out work and added working counter that changes colours according to conditions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -20,16 +20,3 @@ $(document).ready(function() {
     }
   })
 });
-
-
-// $(document).ready(function() {
-//   $("#tweet-text").on("input", function(event) {
-//     const totalCount = $(this).val().length;
-//     $(".counter").text(140 - totalCount);
-//     if (totalCount >= 140) {
-//       $(".counter").addClass("negativeCounter")
-//     } else {
-//       $(".counter").removeClass("negativeCounter")
-//     }
-//   })
-// });
